901ee53a6b6c5d1a4510c3a39b6852879d05965e
Mushaheed Syed <mushaheed@creatoros.co>
2025-06-25 12:57:04 +0530

    Update config

--- expo-plugin/src/android/install_reclaim_project_build_gradle.ts
+++ expo-plugin/src/android/install_reclaim_project_build_gradle.ts
@@ -5,8 +5,8 @@ import {
 
 export const installReclaimProjectBuildGradle: ConfigPlugin = (config) => {
     return withProjectBuildGradle(config, async (config) => {
-        const flutterStorageUrl = process.env.EXPO_PUBLIC_FLUTTER_STORAGE_BASE_URL || config.android['FLUTTER_STORAGE_BASE_URL'] || "https://storage.googleapis.com"
-        const reclaimStorageUrl = process.env.EXPO_PUBLIC_RECLAIM_STORAGE_BASE_URL || config.android['RECLAIM_STORAGE_BASE_URL'] || "https://reclaim-inapp-sdk.s3.ap-south-1.amazonaws.com/android/repo"
+        const flutterStorageUrl = process.env.EXPO_PUBLIC_FLUTTER_STORAGE_BASE_URL || "https://storage.googleapis.com"
+        const reclaimStorageUrl = process.env.EXPO_PUBLIC_RECLAIM_STORAGE_BASE_URL || "https://reclaim-inapp-sdk.s3.ap-south-1.amazonaws.com/android/repo"
 
         if (!config.modResults.contents.match('reclaim-inapp-sdk')) {
             config.modResults.contents = config.modResults.contents.replace(
